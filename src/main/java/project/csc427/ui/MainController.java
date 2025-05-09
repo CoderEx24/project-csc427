fba122f65a74f07203346de15cb1a637b1317da2
CoderEx24 <coderex24@protonmail.com>
2025-05-09 19:42:29 +0300

    src: added simple MainController

---
+++ src/main/java/project/csc427/ui/MainController.java
@@ -0,0 +1,17 @@
+package project.csc427.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
+public class MainController {
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    void onCreateLabel(ActionEvent event) {
+
+    }
+
+}
