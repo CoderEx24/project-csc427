48e5e86707364ebef6379c54a7bb5ccba098de81
CoderEx24 <coderex24@protonmail.com>
2025-05-14 08:19:03 +0300

    ui: simple skeleton implementation of label

--- src/main/java/project/csc427/ui/MainController.java
+++ src/main/java/project/csc427/ui/MainController.java
@@ -1,8 +1,11 @@
 package project.csc427.ui;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.SelectionMode;
+import javafx.event.ActionEvent;
 
 public class MainController {
 
@@ -10,8 +13,21 @@ public class MainController {
     private Canvas canvas;
 
     @FXML
-    void onCreateLabel(ActionEvent event) {
+    private TreeView<String> componentTree;
 
+    @FXML
+    private void initialize() {
+        componentTree.setRoot(new TreeItem<>(""));
+        componentTree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        componentTree.setShowRoot(false);
+    }
+
+    @FXML
+    void onNewLabel(ActionEvent evt) {
+        var root = componentTree.getSelectionModel().getSelectedItem();
+        root = root == null ? componentTree.getRoot() : root;
+
+        root.getChildren().add(new TreeItem<>("Label: newLabel"));
     }
 
 }
